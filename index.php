ab1a5b78606f21bba1152e2ea657a33a049f66d3
jose <jriverabetanzo@gmail.com>
2016-10-25 12:18:06 -0300

    se agrego texto html

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php 
-    echo "Hola Mundo";
-    echo "holi";
-?>
+<doctype />
+<html>
+    <head>
+    
+    </head>
+    <body>
+        
+        <h1><?php echo 'Hola Mundo<br>'?></h1>
+        <?php 
+            for($i = 2; $i < 6 ; $i++){
+                echo "<h$i>¿Como estás?</h$i>";
+            }
+        ?>
+    </body>
+</html>
